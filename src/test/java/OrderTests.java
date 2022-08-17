41b146f06c3353acd0476a46b3e7ed92cf4fbd52
Alina Gainetdinova <malvinaalina@yandex.ru>
2022-08-17 16:42:02 +0500

    order tests

---
+++ src/test/java/OrderTests.java
@@ -0,0 +1,57 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OrderTests {
+    private WebDriver driver;
+
+    @Before
+    public void startUp() {
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+    }
+
+    @Test
+    public void topButtonOrderTest(){
+        HomePageModel homePageModel = new HomePageModel(driver);
+        homePageModel.clickOrderTopButton();
+
+        OrderPageModel orderPageModel = new OrderPageModel(driver);
+        orderPageModel.setFirstForm("Порфирий", "Петрович", "Москва 1", "Чертан", "+9991234567");
+        orderPageModel.nextClick();
+
+        orderPageModel.setSecondForm(2, false, "Привезите, пожалуйста!");
+        orderPageModel.clickOrderButton();
+
+        orderPageModel.clickConfirmButton();
+
+        orderPageModel.checkSuccessMessage();
+    }
+
+    @Test
+    public void bottomButtonOrderTest(){
+        HomePageModel homePageModel = new HomePageModel(driver);
+        homePageModel.clickOrderBottomButton();
+
+        OrderPageModel orderPageModel = new OrderPageModel(driver);
+        orderPageModel.setFirstForm("Имя", "Фамилия", "Москва 2", "Черкиз", "+1231255516");
+        orderPageModel.nextClick();
+
+        orderPageModel.setSecondForm(3, true, "");
+        orderPageModel.clickOrderButton();
+
+        orderPageModel.clickConfirmButton();
+
+        orderPageModel.checkSuccessMessage();
+    }
+
+    @After
+    public void cleanUp() {
+        driver.quit();
+    }
+}
